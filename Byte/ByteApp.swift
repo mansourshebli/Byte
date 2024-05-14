7f13b4cd41e38e66b2ebc8c7f9ec61a675627e2f
Mansour Alshebli <mansour.m.alshebli@gmail.com>
2024-05-14 18:18:22 +0400

    Initial Commit

---
+++ Byte/ByteApp.swift
@@ -0,0 +1,17 @@
+//
+//  ByteApp.swift
+//  Byte
+//
+//  Created by Mansour Mohammed Alshebli on 14/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct ByteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Byte/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Byte
+//
+//  Created by Mansour Mohammed Alshebli on 14/05/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
